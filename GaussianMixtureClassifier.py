8ec7b7fa5a119e09872695fd26d72ac0bb5cefcc
shereenfathy <31906452+shereenfathy@users.noreply.github.com>
2018-03-29 00:57:59 +0200

    GaussianMixtureClassifier
    example :
    import GaussianMixtureClassifier
    import numpy as np
    Data=np.load('data.npy')
    print(GaussianMixtureClassifier.GaussianMixtureClassifier(Data))

---
+++ GaussianMixtureClassifier.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+def ProbabilityDensityFunction(x,mean,variance):
+    pdf=1
+    for i in range(0,x.size,1):
+     pdf=pdf*(1/math.sqrt(2*math.pi*variance[i]))*math.exp((-1*math.pow(x[i]-mean[i],2))/(2*variance[i]))
+    return pdf
+
+def GaussianMixtureClassifier(Data):
+    NumberOFSamples=Data.__len__()
+    gmm = GaussianMixture(n_components=2)
+    gmm.fit(Data)
+    DataClutserNumber=[]
+    for sample in range(0,NumberOFSamples,1):
+     Max=0
+     idx=0
+     for cluster in range(0,2,1):
+        variance=gmm.covariances_[cluster].diagonal()
+        pro=ProbabilityDensityFunction(Data[sample],gmm.means_[cluster],variance)
+        if(pro>Max):
+           Max=pro
+           idx=cluster+1
+     DataClutserNumber.append(idx)
+    return DataClutserNumber
+
+
+
